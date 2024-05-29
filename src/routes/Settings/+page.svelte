e8df5caa01fdaf8b5ef776a879e9bb4604f7b7eb
Curnic Dorin <157069618+C-Dorin@users.noreply.github.com>
2024-05-29 12:46:48 +0300

    Designed Settings page: 3 Settings now (laguage, currency, dark mode)

--- src/routes/Settings/+page.svelte
+++ src/routes/Settings/+page.svelte
@@ -1,10 +1,44 @@
 <script>
 </script>
 
-<div class="flex flex-col text-center">
-	<p class="text-6xl">Settings page</p>
-	<p class="pt-36 text-2xl">In Work 🔨</p>
+<div class="p-2">
+	<div class="p-2 px-40 pt-4 space-y-4">
+		<div
+			class="space-x-4 flex flex-col p-4 text-center border border-LT rounded-lg columbia-blue_BG"
+		>
+			<div class="flex flex-col">
+				<div class="text-4xl font-bold pb-4 border-b border-LT">
+					<p>Settings</p>
+				</div>
+				<div class="flex justify-between items-center text-lg py-4 border-b border-LT">
+					<p class="">Language:</p>
+					<button class="flex border-LT cell content-Light_BG"> English </button>
+					<button class="pr-8">Save</button>
+				</div>
+				<div class="flex justify-between items-center text-lg py-4 border-b border-LT">
+					<p class="">Currency:</p>
+					<button class="flex border-LT cell content-Light_BG"> USD - $ </button>
+					<button class="pr-8">Save</button>
+				</div>
+				<div class="flex justify-between items-center text-lg py-4">
+					<p class="">Dark mode:</p>
+					<button class="flex border-LT cell content-Light_BG"> Off </button>
+					<button class="pr-8">Save</button>
+				</div>
+			</div>
+		</div>
+	</div>
 </div>
 
 <style>
+	.cell {
+		font-size: 1.25rem; /* 20px */
+		line-height: 1.75rem; /* 28px */ /* text-xl */
+		border-width: 1px; /* border */
+		border-radius: 0.375rem; /* 6px */ /* rounded-md */
+		padding: 0.25rem; /* 4px */ /* p-1 */
+		padding-left: 0.5rem; /* 8px */ /* pl-2 */
+		font-weight: 300; /* font-light */
+		width: 60%;
+	}
 </style>
